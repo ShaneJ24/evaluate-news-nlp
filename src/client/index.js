33c82f50ff9d18aec9348205232b28546dc7467a
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2020-12-02 19:17:48 -0500

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import { checkForName } from './js/urlChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkFORurl);
+
+alert("I EXIST")
+console.log("I am working properly!!");
+
+export { checkFORurl, handleSubmit};
