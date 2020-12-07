cb32efeb3ffc8ed26b033a5aa90d78a5c9b20b12
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2020-12-07 08:52:09 -0500

    Delete index.js

--- src/client/index.js
+++
@@ -1,9 +0,0 @@
-import { checkForName } from './js/urlChecker'
-import { handleSubmit } from './js/formHandler'
-
-console.log(checkFORurl);
-
-alert("I EXIST")
-console.log("I am working properly!!");
-
-export { checkFORurl, handleSubmit};
