6dd8449743cb47584d995180467e606d7a2a72c6
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2020-12-08 05:42:13 -0500

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,6 @@
 import { validateURL } from './urlChecker'
 import { handleSubmit, postData } from './js/formHandler'
 
-document.getElementById("results").innerHTML=newData;
 alert("Hi! This is my natural language app");
 console.log("I am working!");
 
