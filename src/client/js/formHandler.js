20047df678b7099b5989e9639abd5272c5345337
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2020-12-14 15:58:57 -0500

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,33 +1,47 @@
 function handleSubmit(event) {
+  console.log('[handleSubmit]', event, event.target)
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById('name').value
+  Client.validateURL(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/article')
-    .then(res => {
-      return res.json()
-    })
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  console.log("::: Form Submitted :::")
+    
+  postData( formText )
+    
+  .then (updateUI()
+  )
 }
+export {handleSubmit}
 
-export { handleSubmit }
 
+    // fetch("http://localhost:8080/newsarticle", {
+    //   method: "POST",
+    //   cache: "no-cache",
+    //   credentials: "same-origin",
+    //   headers: {
+    //     "Content-Type": "text/plain",
+    //   },
+    //   body: formText,
+    // })
+    //   .then((res) => res.json())
+
+    //     document.getElementById("Confidence").innerHTML = `Confidence: ${res.confidence}`;
+    //     document.getElementById("Irony").innerHTML = `Irony: ${res.irony}`;
+    //     document.getElementById("Subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+        
 
 //Function to send the data
 const postData = async(url = '') => {
-    const response = await fetch('http://localhost:8081/article', {
-      method: 'POST',
-      credentials: 'same-origin',
-      mode: 'cors',
-      headers: {
-         'Content-Type': 'text/plain',
+  const response = await fetch('http://localhost:8081/article', {
+    method: 'POST',
+    credentials: 'same-origin',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'text/plain',
       },
-      body: url,
+    body: url,
     });
     
     try {
@@ -37,12 +51,17 @@ const postData = async(url = '') => {
     } catch (error) {
       console.log('error', error);
     }
-  }
   
-  //Update UI with the data from the Meaningcloud API
-  function updateUI(data) {
-    console.log(data)
-    polarity.innerHTML = `Polarity: ${data.Polarity}`;
-    confidence.innerHTML = `Subjectivity: ${data.Subjectivity}`;
-    subjectivity.innerHTML = `Irony: ${data.Irony}`;
+  
+//Update UI with the data from the Meaningcloud API
+const updateUI = async () => {
+  const request = await fetch('/newsarticle');
+  try{
+    const allData = await request.json();
+    document.getElementById('Confidence').innerHTML = allData.confidence;
+    document.getElementById('Irony').innerHTML = allData.irony;
+    document.getElementById('Subjectivity').innerHTML = allData.subjectivity;
+  
+  }catch(error){
+    console.log("error", error);
   }
