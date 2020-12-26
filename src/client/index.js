ed251a9c4a4bb381905c7b72015fe7cc2dcc8b17
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2020-12-26 11:55:12 -0500

    Delete index.js

--- src/client/index.js
+++
@@ -1,7 +0,0 @@
-import { validateURL } from './js/urlChecker.js'
-import { handleSubmit, postData } from './js/formHandler.js'
-
-alert("Hi! This is my natural language app");
-console.log("I am working!");
-
-export { validateURL, handleSubmit};
