fc7e4930cc0799b4b093ec8a661d945468c3c249
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2021-01-11 19:45:09 -0500

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 import { validateURL } from './js/urlChecker.js'
 import { handleSubmit, postData } from './js/formHandler.js'
+import 'evaluate-news-nlp/src/client/styles/base.scss';
+import 'evaluate-news-nlp/src/client/styles/form.scss';
+import 'evaluate-news-nlp/src/client/styles/header.scss';
+import 'evaluate-news-nlp/src/client/styles/resets.scss';
 
 alert("Hi! This is my natural language app");
 console.log("I am working!");
